caaa76f33414f0694301611810ea6c9295966dd0
JaviCaiola <javiteen1250@gmail.com>
2023-12-09 18:13:06 -0300

    primer slide

--- index.js
+++ index.js
@@ -1,24 +1,73 @@
 const leftCard = document.querySelector('#left');
 const mediumCard = document.querySelector('#medium');
-const rightCard = document.querySelector('#rigth');
+const rigthCard = document.querySelector('#rigth');
+const contentLeft = document.querySelector('#content-left');
+const contentMedium = document.querySelector('#content-medium');
+const contentRigth = document.querySelector('#content-rigth');
 const card = document.querySelectorAll('.card');
 
+leftCard.addEventListener('mouseenter', (event) =>{
+    mediumCard.classList.add('normal-size');
+    mediumCard.classList.add('index-two');
+    mediumCard.classList.remove('medium');
+    leftCard.classList.add('index-card');
 
-function handleCardHover(enteredCard, addClass, removeClass) {
-  mediumCard.classList.add('normal-size');
-  mediumCard.classList.remove('medium');
-  enteredCard.classList.add('index-card');
-  enteredCard.addEventListener('mouseleave', function() {
-    mediumCard.classList.add('medium');
+    contentLeft.classList.remove('oculto');
+    leftCard.classList.remove('card_not');
+    contentMedium.classList.add('oculto');
+    mediumCard.classList.add('card_not');
+
+    mediumCard.classList.remove('index-card');
+    leftCard.classList.add('index-card');
+})
+
+leftCard.addEventListener('mouseleave', (event) =>{
     mediumCard.classList.remove('normal-size');
-    enteredCard.classList.remove('index-card');
-  });
-}
+    mediumCard.classList.remove('index-two');
+    mediumCard.classList.add('medium');
+    leftCard.classList.remove('index-card');
+    
+    contentLeft.classList.add('oculto');
+    leftCard.classList.add('card_not');
+    contentMedium.classList.remove('oculto');
+    mediumCard.classList.remove('card_not');
 
-leftCard.addEventListener('mouseenter', function() {
-  handleCardHover(leftCard, 'index-card', 'normal-size');
-});
+    
+    mediumCard.classList.add('index-card');
+    leftCard.classList.remove('index-card');
+})
+rigthCard.addEventListener('mouseenter', (event) =>{
+    mediumCard.classList.add('normal-size')
+    mediumCard.classList.remove('medium');
+    rigthCard.classList.add('index-card');
+    rigthCard.classList.remove('card_not');
+    contentRigth.classList.remove('oculto')
 
-rightCard.addEventListener('mouseenter', function() {
-  handleCardHover(rightCard, 'index-card', 'normal-size');
-});
+    contentLeft.classList.remove('oculto');
+    leftCard.classList.remove('card_not');
+    contentMedium.classList.add('oculto');
+    mediumCard.classList.add('card_not');
+
+    mediumCard.classList.remove('index-card');
+    rigthCard.classList.add('index-card');
+
+    contentLeft.classList.add('oculto');
+    leftCard.classList.add('card_not')
+})
+
+rigthCard.addEventListener('mouseleave', (event) =>{
+    mediumCard.classList.remove('normal-size')
+    mediumCard.classList.add('medium');
+    rigthCard.classList.remove('index-card');
+    rigthCard.classList.add('card_not');
+    contentRigth.classList.add('oculto');
+
+    contentLeft.classList.add('oculto');
+    leftCard.classList.add('card_not');
+    contentMedium.classList.remove('oculto');
+    mediumCard.classList.remove('card_not');
+
+    mediumCard.classList.add('index-card');
+    rigthCard.classList.remove('index-card');
+
+})
